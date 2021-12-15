2aa84027bb6cdf78f18ba709eb5b180e4b6faebb
kumapagu <kmno_555@yahoo.co.jp>
2021-12-16 00:07:00 +0900

    モデルの単体テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    # Faker::Config.locale = :ja
+    nickname                  {Faker::Name.initials(number: 2)}
+    email                     {Faker::Internet.free_email}
+    password                  {Faker::Internet.password(min_length: 6, mix_case: true)}
+    password_confirmation     {password}
+    birthday                  {Faker::Date.birthday(min_age: 10, max_age: 60).strftime('%Y-%m-%d')}
+    first_name                {"太郎"}
+    last_name                 {"山田"}
+    first_name_kana           {"タロウ"}
+    last_name_kana            {"ヤマダ"}
+  end
+end
