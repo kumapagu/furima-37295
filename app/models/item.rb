23c5e20f36865541fb15ce29ce567f7ff4ce8076
kumapagu <kmno_555@yahoo.co.jp>
2021-12-16 18:33:17 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
 
+  validates :name, presence: true
+  validates :item_description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :ship_days_id, presence: true
+  validates :price, presence: true
+  validates_inclusion_of :price, in:300..9999999
+  validates :user_id, presence: true
+  validates :image, presence: true
+
+
   belongs_to :user
-  extend ActiveHash::Associations
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_charge
