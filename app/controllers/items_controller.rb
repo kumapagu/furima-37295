5d7dfee73635eade7ae4a886b37fdf117d1d0156
kumapagu <kmno_555@yahoo.co.jp>
2021-12-16 15:36:38 +0900

    form_withとストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    item = Item.new(item_params)
   end
-  
+
+
+  private
+
+  def item_params
+    paramas.repuire(:item).permit(:name, :item_description, :cotegory_id, :status_id, :delivery_charge_id, :prefecture_id, :ship_days_id).merge(user_id: current_user.id)
+  end
 end
