6482529fda0b197f6d8cd9a4e6de6b448682414f
kumapagu <kmno_555@yahoo.co.jp>
2021-12-16 15:24:19 +0900

    アソシエーションの追加

--- app/models/cotegory.rb
+++ app/models/cotegory.rb
@@ -11,5 +11,7 @@ Class Cotegory < Activehash::Base
     { id: 9, name: "スポーツ・レジャー" }
     { id: 10, name: "ハンドメイド" }
     { id: 11, name: "その他" }
-    
+  ]
+   include ActiveHash::Associations
+   has_many :items 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  extend ActiveHash::Associations
+  belongs_to :cotegory
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :ship_day
 end
