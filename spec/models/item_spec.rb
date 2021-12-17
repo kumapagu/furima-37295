31929ed7c4ad9404a8d86a6eae5eed08dce098a8
kumapagu <kmno_555@yahoo.co.jp>
2021-12-17 20:26:39 +0900

    priceが300未満、userが紐付いていないのテストコードを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -100,10 +100,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
 
-      it 'priceの数値が300未満、10000000以上では出品できない' do
+      it 'priceの数値が10000000以上では出品できない' do
         @item.price = '10000000'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
+      end
+
+      it 'priceの数値が300未満では出品できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
+      end
+
+      it 'userが紐付いていなければ登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
 
       it 'imageが空では登録できない' do
