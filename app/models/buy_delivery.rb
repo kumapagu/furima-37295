effa7a4d5e73a3ac522514d60505c0bc73eaa0dd
kumapagu <kmno_555@yahoo.co.jp>
2021-12-20 21:43:13 +0900

    バリデーションの設定

--- app/models/buy_delivery.rb
+++ app/models/buy_delivery.rb
@@ -2,4 +2,13 @@ class BuyDelivery
   include ActiveModel::Model
   att_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :buy_id
 
-  
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :block
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :buy_id
+  end
