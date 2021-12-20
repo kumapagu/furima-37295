495d921c8e027026284171399dd6f6e0b5bc0160
kumapagu <kmno_555@yahoo.co.jp>
2021-12-20 22:36:21 +0900

    ストロングパラメーターと保存時のページ遷移の分岐設定

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,22 @@
 class BuysController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @buy_delivery = BuyDelivery.new
   end
 
   def create
+    @buy_delivery = BuyDelivery.new(buy_params)
+    if @buy_delivery.valid?
+      @buy_delivery.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  def buy_params
+    params.require(:buy_delivery).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
