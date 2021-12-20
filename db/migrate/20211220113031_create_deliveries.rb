be5b7dd20e53b4b297b7b5bd2f5c8399e894b723
kumapagu <kmno_555@yahoo.co.jp>
2021-12-20 20:43:13 +0900

    buysテーブルとdeliveriesテーブルのマイグレーション

--- db/migrate/20211220113031_create_deliveries.rb
+++ db/migrate/20211220113031_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-
+      t.string :post_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :block, null: false
+      t.string :building
+      t.string :phone_number, null:false
+      t.references :buy, null: false, foreign_key: true
       t.timestamps
     end
   end
