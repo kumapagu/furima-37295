0da4d3d0d84c91e8abca3b2435a474e79b214357
kumapagu <kmno_555@yahoo.co.jp>
2021-12-21 18:07:17 +0900

    クレジットカード機能の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -5,8 +5,15 @@ class BuysController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @buy_delivery = BuyDelivery.new(buy_params)
     if @buy_delivery.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buy_params[:token],
+        currency: "jpy"
+      )
       @buy_delivery.save
       redirect_to root_path
     else
@@ -17,6 +24,6 @@ class BuysController < ApplicationController
 
   private
   def buy_params
-    params.require(:buy_delivery).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy_delivery).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
